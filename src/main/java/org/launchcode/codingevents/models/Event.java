a4fd9859ba04504612b8f94eb6a9fa0cc6fe18dd
Marcie DeFonce <marcie.defonce@gmail.com>
2023-05-22 18:36:02 -0500

    17.5. Studio: Creating an AbstractEntity

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -9,11 +9,7 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
-public class Event {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class Event extends AbstractEntity {
 
     @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 20, message = "Name must bee between 3 and 50 characters long.")
@@ -70,25 +66,9 @@ public class Event {
         this.type = type;
     }
 
-    public int getId() {
-        return id;
-    }
-
     @Override
     public String toString() {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
